70552e78641474f80f9500f618ff3f3426b4eae5
Tyler Cone <tcone@ubicquia.com>
2018-05-07 13:15:06 -0400

    Changed commands to be new variables and path to my github

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM arm32v6/golang:1.10.1-alpine3.7 AS builder
 ENV GOPATH /go
 WORKDIR /go/src
 
-RUN mkdir -p /go/src/github.com/cjimti/iotwifi
-COPY . /go/src/github.com/cjimti/iotwifi
+RUN mkdir -p /go/src/github.com/herrflockig/iotwifi
+COPY . /go/src/github.com/herrflockig/iotwifi
 
-RUN CGO_ENABLED=0 go build -a -installsuffix cgo -o /go/bin/wifi /go/src/github.com/cjimti/iotwifi/main.go
+RUN CGO_ENABLED=0 go build -a -installsuffix cgo -o /go/bin/wifi /go/src/github.com/herrflockig/iotwifi/main.go
 
 FROM arm32v6/alpine
 
--- iotwifi/commands.go
+++ iotwifi/commands.go
@@ -43,11 +43,11 @@ func (c *Command) AddApInterface() {
 
 // BrideAPtoEth bridges the connection from eth0 to uap0
 func (c *Command) BridgeAPtoEth() {
-    cmd := exec.Command("sed", "-i", "s/#?net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/", "/etc/sysctl.conf")
-    cmd.Run()
-    cmd := exec.Command("sysctl", "-p")
-    cmd.Run()
-    iptables_args := []string{
+    cmd_sed := exec.Command("sed", "-i", "s/#?net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/", "/etc/sysctl.conf")
+    cmd_sed.Run()
+    cmd_sysctl := exec.Command("sysctl", "-p")
+    cmd_sysctl.Run()
+    iptables1_args := []string{
         "-t",
         "nat",
         "-A",
@@ -55,20 +55,20 @@ func (c *Command) BridgeAPtoEth() {
         "--out-interface",
         "uap0",
         "-j",
-        "MASQUERADE"
+        "MASQUERADE",
     }
-    cmd := exec.Command("iptables", iptables_args...)
-    cmd.Run()
-    iptables_args := []string{
+    cmd_iptables1 := exec.Command("iptables", iptables1_args...)
+    cmd_iptables1.Run()
+    iptables2_args := []string{
         "-A",
         "FORWARD",
         "--in-interface",
         "eth0",
         "-j",
-        "ACCEPT"
+        "ACCEPT",
     }
-    cmd := exec.Command("iptables", iptables_args...)
-    cmd.Run()
+    cmd_iptables2 := exec.Command("iptables", iptables2_args...)
+    cmd_iptables2.Run()
 }
 
 // CheckInterface checks the AP interface.
