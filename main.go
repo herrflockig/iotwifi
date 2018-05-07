572039350947a595320a7a95d86b40fd1b97e53e
Tyler Cone <tcone@ubicquia.com>
2018-05-07 13:17:05 -0400

    changed path in main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/bhoriuchi/go-bunyan/bunyan"
-	"github.com/cjimti/iotwifi/iotwifi"
+	"github.com/herrflockig/iotwifi/iotwifi"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
